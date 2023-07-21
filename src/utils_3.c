911068ddbeb74bc67cc34fdb6246c8295858a27b
Bilel MOUDACHE <bmoudach@paul-f4Br6s1.clusters.42paris.fr>
2023-07-21 16:02:47 +0200

    ok

--- src/utils_3.c
+++ src/utils_3.c
@@ -6,7 +6,7 @@
 /*   By: bmoudach <bmoudach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:16:54 by bmoudach          #+#    #+#             */
-/*   Updated: 2023/07/21 15:49:48 by bmoudach         ###   ########.fr       */
+/*   Updated: 2023/07/21 16:02:13 by bmoudach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,14 @@ void	move(t_data *data, int x, int y)
 {
 	if (data->map[data->pos_py + y][data->pos_px + x] == '1')
 		return ;
+    else if(data->map[data->pos_py + y][data->pos_px + x] == 'E' && data->count_c > 0)
+        return;
 	else
 	{
+	    if (data->map[data->pos_py + y][data->pos_px + x] == 'C')
+            data->count_c--;
+	    if (data->map[data->pos_py + y][data->pos_px + x] == 'E' && data->count_c == 0)
+            exit_and_free(data);          
 		data->map[data->pos_py][data->pos_px] = '0';
 		data->map[data->pos_py + y][data->pos_px + x] = 'P';
 		data->pos_px = data->pos_px + x;
